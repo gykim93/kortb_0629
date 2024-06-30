8afb213f1cc65d35c79a8fd88ed9eca06d35eefe
gykim93 <gykim1993@gmail.com>
2024-06-30 12:05:12 +0900

    2단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -7,7 +7,7 @@ class App {
         System.out.println("프로그램 실행");
         System.out.println("== 명언 앱 ==");
 
-        while (true){
+        while (true) {
             System.out.print("명령) ");
 
             Scanner scanner = new Scanner(System.in);
@@ -16,9 +16,15 @@ class App {
             if (cmd.equals("종료")) {
                 break;
             }
+            else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
 
+                System.out.print("작가 : ");
+                String authorName = scanner.nextLine();
 
-            System.out.printf("입력하신 명령 : %s\n", cmd);
+                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
+            }
         }
     }
 }
