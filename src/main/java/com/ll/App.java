24a52a109b7036f987bd39232def28fbb1757844
gykim1993@gmail.com <gykim1993@gmail.com>
2024-07-06 13:05:37 +0900

    5단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -14,14 +14,13 @@ class App {
 
         while (true) {
             System.out.print("명령) ");
-            
+
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine().trim(); // 명령어 입력 시에 공백 제거
 
             if (cmd.equals("종료")) {
                 break;
-            }
-            else if (cmd.equals("등록")) {
+            } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
                 String content = scanner.nextLine().trim(); // 명언 입력 시 공백 제거
 
@@ -34,10 +33,19 @@ class App {
 
                 Quotation quotation = new Quotation(id, content, authorName);
                 quotations.add(quotation);
-                System.out.printf("%d번 명언이 등록되었습니다.\n",lastQuotatinId);
-            }
-            else if (cmd.equals("목록")) {
-                System.out.println("총 갯수 : " + quotations.size());
+                System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotatinId);
+
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-----------------------");
+
+                if (quotations.isEmpty()) {
+                    System.out.println("등록된 명언이 없습니다.");
+                }
+                for (int i = quotations.size() - 1; i >= 0; i--) {
+                    Quotation quotation = quotations.get(i);
+                    System.out.printf("%d / %s/ %s\n", quotation.id, quotation.authorName, quotation.content);
+                }
             }
         }
     }
