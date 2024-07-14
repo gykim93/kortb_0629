917f62250538ef56b85df966c2a45ce162805354
gykim93 <gykim1993@gmail.com>
2024-07-14 11:42:31 +0900

    Rq 적용, if문을 switch 문으로 교체, 기존 getParamAsInt 함수 제거

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class App {
+public class App {
     Scanner scanner;
     int lastQuotationId;
     List<Quotation> quotations;
@@ -18,31 +18,28 @@ class App {
     void run() {
         System.out.println("== 명언 앱 ==");
 
-
-        List<Quotation> quotations = new ArrayList<>();
-
         while (true) {
-            System.out.println("명령) ");
+            System.out.print("명령) ");
 
             String cmd = scanner.nextLine();
 
             Rq rq = new Rq(cmd);
 
-            System.out.println("rq.getAction : " + rq.getAction());
-            System.out.println("rq.getParamAsInt : " + rq.getParamAsInt("id", 0));
-
-            if (cmd.equals("종료")) {
-                break;
-            } else if (cmd.equals("등록")) {
-                actionWrite();
-                System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
-
-            } else if (cmd.equals("목록")) {
-                actionList();
-            } else if (cmd.startsWith("삭제?")) {
-                actionRemove(cmd);
-            } else if (cmd.startsWith("수정?")) {
-                actionModify(cmd);
+            switch (rq.getAction()) {
+                case "종료":
+                    return;
+                case "등록":
+                    actionWrite();
+                    break;
+                case "목록":
+                    actionList();
+                    break;
+                case "삭제":
+                    actionRemove(rq);
+                    break;
+                case "수정":
+                    actionModify(rq);
+                    break;
             }
         }
     }
@@ -55,11 +52,12 @@ class App {
         String authorName = scanner.nextLine();
 
         lastQuotationId++;
-
         int id = lastQuotationId;
 
         Quotation quotation = new Quotation(id, content, authorName);
         quotations.add(quotation);
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
     }
 
     void actionList() {
@@ -67,59 +65,34 @@ class App {
 
         System.out.println("----------------------");
 
-        if (quotations.isEmpty()) System.out.println("등록된 명언이 없습니다.");
+        if (quotations.isEmpty())
+            System.out.println("등록된 명언이 없습니다.");
 
         for (int i = quotations.size() - 1; i >= 0; i--) {
             Quotation quotation = quotations.get(i);
             System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
         }
-    } //"1"
-
-    void actionRemove(String cmd) {
-        int id = getParamAsint(cmd, "id", 0);
-        if (id == 0) {
-            System.out.println("id를 정확히 입력해주세요.");
-            return; //함수를 끝낸다.
-        }
-
-        System.out.printf("%d번 명언을 삭제합니다\n", id);
     }
 
-    void actionModify(String cmd) {
-        int id = getParamAsint(cmd, "id", 0);
+    void actionRemove(Rq rq) {
+        int id = rq.getParamAsInt("id", 0);
+
         if (id == 0) {
             System.out.println("id를 정확히 입력해주세요.");
-            return; //함수를 끝낸다.
+            return; // 함수를 끝낸다.
         }
 
-        System.out.printf("%d번 명언을 수정합니다\n", id);
+        System.out.printf("%d번 명언을 삭제합니다.\n", id);
     }
 
-    int getParamAsint(String cmd, String paramName, int defaultValue) {
-        String[] cmdBits = cmd.split("\\?", 2);
-        String queryString = cmdBits[1];
+    void actionModify(Rq rq) {
+        int id = rq.getParamAsInt("id", 0);
 
-        String[] queryStringBits = queryString.split("&");
-
-        int id = 0;
-        for (int i = 0; i < queryStringBits.length; i++) {
-            String queryParamStr = queryStringBits[i];
-
-            String[] queryParamStrBits = queryParamStr.split("=", 2);
-
-            String _paramName = queryParamStrBits[0];
-            String paramValue = queryParamStrBits[1];
-
-            if (_paramName.equals(paramName)) {
-                try {
-                    // 문제가 없을 경우
-                    return Integer.parseInt(paramValue);  //return Integer.parseInt("123123werewr");
-                } catch (NumberFormatException e) {
-                    // 문제가 생긴경우
-                    return defaultValue;
-                }
-            }
+        if (id == 0) {
+            System.out.println("id를 정확히 입력해주세요.");
+            return; // 함수를 끝낸다.
         }
-        return defaultValue;
+
+        System.out.printf("%d번 명언을 수정합니다.\n", id);
     }
 }
--- src/main/java/com/ll/Rq.java
+++ src/main/java/com/ll/Rq.java
@@ -33,19 +33,22 @@ public class Rq {
             paramValues.add(paramValue);
         }
     }
+
     String getAction() {
         return action;
     }
-    public int getParamAsInt(String paramName, int defaltValue){
+
+    public int getParamAsInt(String paramName, int defaultValue) {
         int index = paramNames.indexOf(paramName);
 
-        if (index == -1) return defaltValue;
+        if (index == -1) return defaultValue;
+
         String paramValue = paramValues.get(index);
 
         try {
             return Integer.parseInt(paramValue);
-        }catch (NumberFormatException e){
-            return defaltValue;
+        } catch (NumberFormatException e) {
+            return defaultValue;
         }
     }
 }
